35a2819a7becf3b42fedaa31098f64ea986dc180
ChristianLaptop <ChristianLaptop@Christian.home>
2013-11-14 02:09:06 +0100

    Færdig

--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -7,11 +7,7 @@ import org.lwjgl.input.Keyboard;
 
 public class Menu extends BasicGameState {
 
-	// private String mouse = "No input yet";
-	
-//	private Image face;
-//	private int faceX = 200;
-//	private int faceY = 200;
+	private Image playNow, exitGame;
 
 	public Menu(int state) {
 
@@ -19,53 +15,37 @@ public class Menu extends BasicGameState {
 
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-//		face = new Image("res/bubble_right.png");
+		playNow = new Image("res/playNow.png");
+		exitGame = new Image("res/exitGame.png");
 
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		g.fillOval(75,100,100,100);
-		g.drawString("Play now!",80,70);
-		
-//		g.drawImage(face,faceX,faceY);
-		
-		// g.drawString(mouse, 50, 50);
-		// g.drawRect(50, 100, 60, 120);
-		//
-		// Image img = new Image("res/bubble_right.png");
-		// g.drawImage(img, 200, 130);
+		g.drawString("Welcome to Happy Land!", 100, 50);
+		playNow.draw(100, 100);
+		exitGame.draw(100, 200);
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
+
+		int posX = Mouse.getX();
+		int posY = Mouse.getY();
+
+		//play button
+		if ((posX > 100 && posX < 311) && (posY > 209 && posY < 260)) {
+			if(Mouse.isButtonDown(0)){
+				sbg.enterState(1);
+			}
+		}
 		
-		Input input = gc.getInput();
-		
-	
-		 int xPos = Mouse.getX();
-		 int yPos = Mouse.getY();
-		 
-		 if((xPos>75 && xPos<175)&&(yPos>160 && yPos<260)){
-			 if(input.isMouseButtonDown(0)){
-				 sbg.enterState(1);
-			 }
-		 }
-//		 mouse = "Mouse postion x: " + xPos + " Y: " + yPos;
-		
-//		Input input = gc.getInput();
-//		if(input.isKeyDown(Input.KEY_UP)){
-//			faceY -= 1;
-//		}
-//		if(input.isKeyDown(Input.KEY_DOWN)){
-//			faceY += 1;
-//		}
-//		if(input.isKeyDown(Input.KEY_LEFT)){
-//			faceX -= 1;
-//		}
-//		if(input.isKeyDown(Input.KEY_RIGHT)){
-//			faceX += 1;
-//		}
+		//exit button
+		if ((posX > 100 && posX < 311) && (posY > 109 && posY < 160)) {
+			if(Mouse.isButtonDown(0)){
+				System.exit(0);
+			}
+		}
 	}
 
 	public int getID() {
--- src/javagame/Play.java
+++ src/javagame/Play.java
@@ -3,25 +3,111 @@ package javagame;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-public class Play extends BasicGameState{
+import sun.awt.image.PixelConverter.Rgba;
 
-	public Play(int state){
-		
+public class Play extends BasicGameState {
+
+	private Animation me, movingUp, movingDown, movingLeft, movingRight;
+	private Image worldMap;
+	boolean quit = false;
+	int[] duration = { 200, 200 };
+	float myPositionX = 0;
+	float myPositionY = 0;
+	float shiftX = myPositionX + 360;
+	float shiftY = myPositionY + 160;
+
+	public Play(int state) {
+
 	}
-	
-	public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
-		
+
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		worldMap = new Image("res/world.png");
+		Image[] walkUp = { new Image("res/buckysBack.png"),
+				new Image("res/BuckysBack.png") };
+		Image[] walkDown = { new Image("res/buckysFront.png"),
+				new Image("res/BuckysFront.png") };
+		Image[] walkLeft = { new Image("res/buckysLeft.png"),
+				new Image("res/BuckysLeft.png") };
+		Image[] walkRight = { new Image("res/buckysRight.png"),
+				new Image("res/BuckysRight.png") };
+
+		movingUp = new Animation(walkUp, duration, false);
+		movingDown = new Animation(walkDown, duration, false);
+		movingLeft = new Animation(walkLeft, duration, false);
+		movingRight = new Animation(walkRight, duration, false);
+		me = movingDown;
 	}
-	
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-		g.drawString("This is the Play state",100,100);
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		worldMap.draw(myPositionX, myPositionY);
+		me.draw(shiftX, shiftY);
+		g.drawString("My X:" + myPositionX + "\nMy Y: " + myPositionY, 400, 20);
+
+		if (quit == true) {
+			g.drawString("Resume (R)", 250, 100);
+			g.drawString("Main Menu (M)", 250, 150);
+			g.drawString("Quit (Q)", 250, 200);
+			if (quit == false) {
+				g.clear();
+			}
+		}
+
 	}
-	
-	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
-		
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		Input input = gc.getInput();
+
+		if (input.isKeyDown(Input.KEY_W)) {
+			me = movingUp;
+			myPositionY += delta * .1f;
+			if (myPositionY > 162) {
+				myPositionY -= delta * .1f;
+			}
+		}
+		if (input.isKeyDown(Input.KEY_S)) {
+			me = movingDown;
+			myPositionY -= delta * .1f;
+			if (myPositionY < -600) {
+				myPositionY += delta * .1f;
+			}
+		}
+		if (input.isKeyDown(Input.KEY_A)) {
+			me = movingLeft;
+			myPositionX += delta * .1f;
+			if (myPositionX > 364) {
+				myPositionX -= delta * .1f;
+			}
+		}
+		if (input.isKeyDown(Input.KEY_D)) {
+			me = movingRight;
+			myPositionX -= delta * .1f;
+			if (myPositionX < -800) {
+				myPositionX += delta * .1f;
+			}
+		}
+
+		// escape
+		if (input.isKeyDown(Input.KEY_ESCAPE)) {
+			quit = true;
+		}
+		// When menu is UP
+		if (quit == true) {
+			if (input.isKeyDown(Input.KEY_R)) {
+				quit = false;
+			}
+			if (input.isKeyDown(Input.KEY_M)) {
+				sbg.enterState(0);
+			}
+			if (input.isKeyDown(Input.KEY_Q)) {
+				System.exit(0);
+			}
+		}
 	}
-	
-	public int getID(){
+
+	public int getID() {
 		return 1;
 	}
 }
